73395b8fcd63bd2937e1a8b7fa995593bc45f039
Vincent Chan <vincent.chan@biola.edu>
2017-12-05 16:21:29 -0800

    cors

--- api/routes/dataRoutes.js
+++ api/routes/dataRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
+    next();
   });
 
   app.route('/data')
