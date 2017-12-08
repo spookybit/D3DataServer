d201f2ac60954f5a1961a27681ba169e040111de
Vincent Chan <vincent.chan@biola.edu>
2017-12-07 17:08:17 -0800

    heroku seed file deleted old entries

--- seed.js
+++ seed.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose');
 var IndustryModel = mongoose.model('Industry');
 var Schema = mongoose.Schema;
 
-// mongoose.connect('mongodb://localhost/D3Datadb');
+mongoose.connect('mongodb://localhost/D3Datadb');
 mongoose.connect('mongodb://heroku_skhwf7mp:7b85vve24lthbklgsu8h0eovsa@ds155411.mlab.com:55411/heroku_skhwf7mp');
 
-IndustryModel.remove({});
+Industry.remove({}, function(err, data) {});
 
 industrySeeds.forEach(function(el) {
   let newData = new IndustryModel(el);
